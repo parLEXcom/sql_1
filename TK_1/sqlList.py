6dd13093c5d11f8cb8f4e2b64033386e1a79ff16
lex <LEX327@yandex.ru>
2025-12-01 20:13:49 +0300

    Добавлено обновление баз данных

--- TK_1/sqlList.py
+++ TK_1/sqlList.py
@@ -3,13 +3,16 @@ import sqlite3
 def main():
     conn = sqlite3.connect("my_sql.db")
     cur = conn.cursor()
-    # Создание таблицы с полями
-    #cur.execute("""CREATE TABLE Customers(CustomerID INTEGER PRIMARY KEY NOT NULL, Name TEXT, Email TEXT)""")
+    # Создание таблицы с полями если таковой нет
+    cur.execute("""CREATE TABLE IF NOT EXISTS Customers(CustomerID INTEGER PRIMARY KEY NOT NULL, Name TEXT, Email TEXT)""")
 
     # Создание данных в таблице
-    cur.execute("""INSERT INTO Customers (Name, Email)
-                VALUES ("ser", "ser333@yandex.ru"),
-                        ("vaccv", "vaccv333@yandex.ru")""")
+    #cur.execute("""INSERT INTO Customers (Name, Email)
+                #VALUES ("ser", "ser333@yandex.ru"),
+                        #("vaccv", "vaccv333@yandex.ru")""")
+
+    #Обновление даннах в таблице
+    cur.execute("""UPDATE Customers SET Name = "ccv" WHERE CustomerID = 6 """)
 
     # Получение данныи из таблицы
     cur.execute("SELECT * FROM Customers")
@@ -17,6 +20,8 @@ def main():
 
     for el in sel:
         print(f"ID {el[0]}, Name:{el[1]}, Email--{el[2]}")
+
+
 
 
     conn.commit()
